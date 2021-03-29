631669b811e59a6cd90527b693e2d0c9af175b82
DanteDeGrave <vladimir.system@gmail.com>
2021-03-29 10:43:10 +0500

    fix

--- js/menu-constructor.js
+++ js/menu-constructor.js
@@ -2,46 +2,44 @@ const pageMenuAnchorContainer = document.querySelector('.page-menu__anchors-wrap
 const pageMenuContainer = document.querySelector('.page-menu__menu-wrap');
 
 const renderSectionMenu = (menuObject) => {
-
-    menuObject.forEach((element) => {
-        const section = document.createElement('section');
-        section.classList.add('menu-item');
-        section.id = element.id;
-        section.innerHTML = `<h2 class="menu-item__title  section-title">${element.groupName}</h2>`;
-        renderDishes(section, element);
-        pageMenuContainer.appendChild(section);
-        renderAnchors(element.id,element.groupName);
-    });
+  menuObject.forEach((element) => {
+    const section = document.createElement('section');
+    section.classList.add('menu-item');
+    section.id = element.id;
+    section.innerHTML = `<h2 class="menu-item__title  section-title">${element.groupName}</h2>`;
+    renderDishes(section, element);
+    pageMenuContainer.appendChild(section);
+    renderAnchors(element.id,element.groupName);
+  });
 };
 
 const renderDishes = (section, data) => {
-    data.dishes.forEach((element) => {
-        if (element.subTitle) {
-            section.innerHTML += `<h3 class="menu-item__subtitle">${element.subTitle}</h3>`;
-        } else {
-            if (element.description) {
-                section.innerHTML +=
-                  `<div class="menu-item__menu-line">
-                    <div class="menu-item__dish-name-descr-wrap">
-                      <h4 class="menu-item__dish-name">${element.name}&nbsp<span class="menu-item__dish-weight" aria-label="выход блюда в граммах">${element.weight}</span></h4>
-                      <span class="menu-item__dish-name-descr">${element.description}</span></div>
-                    <span class="menu-item__dish-price" aria-label="цена в рублях">${element.price}</span>
-                  </div>`;
-            } else {
-                section.innerHTML +=
-                  `<div class="menu-item__menu-line">
-                    <div class="menu-item__dish-name-descr-wrap">
-                      <h4 class="menu-item__dish-name">${element.name}&nbsp<span class="menu-item__dish-weight" aria-label="выход блюда в граммах">${element.weight}</span></h4>
-                    </div>
-                    <span class="menu-item__dish-price" aria-label="цена в рублях">${element.price}</span>
-                  </div>`;
-            }
-        }
-    });
+  data.dishes.forEach((element) => {
+    if (element.subTitle) {
+      section.innerHTML += `<h3 class="menu-item__subtitle">${element.subTitle}</h3>`;
+    } else {
+      if (element.description) {
+          section.innerHTML +=
+            `<div class="menu-item__menu-line">
+              <div class="menu-item__dish-name-descr-wrap">
+                <h4 class="menu-item__dish-name">${element.name}&nbsp<span class="menu-item__dish-weight" aria-label="выход блюда в граммах">${element.weight}</span></h4>
+                <span class="menu-item__dish-name-descr">${element.description}</span></div>
+              <span class="menu-item__dish-price" aria-label="цена в рублях">${element.price}</span>
+            </div>`;
+    } else {
+          section.innerHTML +=
+            `<div class="menu-item__menu-line">
+              <div class="menu-item__dish-name-descr-wrap">
+                <h4 class="menu-item__dish-name">${element.name}&nbsp<span class="menu-item__dish-weight" aria-label="выход блюда в граммах">${element.weight}</span></h4>
+              </div>
+              <span class="menu-item__dish-price" aria-label="цена в рублях">${element.price}</span>
+            </div>`;}
+    }
+  });
 };
 
 const renderAnchors = (id, description) => {
-    pageMenuAnchorContainer.innerHTML += `<a class="page-menu__anchors-item" href="menu.html#${id}">${description}</a>`
+  pageMenuAnchorContainer.innerHTML += `<a class="page-menu__anchors-item" href="menu.html#${id}">${description}</a>`
 };
 
 export {renderSectionMenu};
--- js/menu-preview.js
+++ js/menu-preview.js
@@ -1,13 +1,13 @@
 const menuPreviewContainer = document.querySelector('.main-content__menu-links-container');
 
 const renderMenuPreview = (menu) => {
-    menu.forEach((element) => {
-        menuPreviewContainer.innerHTML +=
-            `<a class="main-content__menu-link" href="menu.html#${element.id}">
-                <img src="${element.image}" width="250" height="125">
-                <h3>${element.groupName}</h3>
-             </a>`;
-    });
+  menu.forEach((element) => {
+    menuPreviewContainer.innerHTML +=
+        `<a class="main-content__menu-link" href="menu.html#${element.id}">
+            <img src="${element.image}" width="250" height="125">
+            <h3>${element.groupName}</h3>
+         </a>`;
+  });
 };
 
 export {renderMenuPreview};
